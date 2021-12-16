6bf811544c0b79332d642d18cc856ee51244bac8
Shaik Kamil <shaikm.kamil@gmail.com>
2021-12-15 22:14:06 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// your code here
+function shout(string) {
+    return string.toUpperCase();
+}// your code here
+
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoomate(string){
+    if (string === 'hello'){
+        return ("I can't hear you!");
+    }
+    else if (string === 'HELLO'){
+    return ("YES INDEED!")
+    }
+    else if (string ==="Let's have dinner together!"){
+        return("I would love to!");
+    }
+}
